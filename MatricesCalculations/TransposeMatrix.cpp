70a793f2aa58a62430b7be62a4bbdaadf399966d
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-04-10 15:47:46 +0800

    Update TransposeMatrix.cpp

--- MatricesCalculations/TransposeMatrix.cpp
+++ MatricesCalculations/TransposeMatrix.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int p[10][10], row, column, i, j, trans[10][10];
+    cout << "Enter rows of matrix: ";
+    cin >> row;
+    cout << "Enter column of matrix: ";
+    cin >> column;
+    cout << endl << "Enter elements of matrix: " << endl;
+    for(i = 0; i < row; ++i)
+    for(j = 0; j < column; ++j)
+    {
+        cout << "Enter elements P" << i + 1 << j + 1 << ": ";
+        cin >> p[i][j];
+    }
+    cout << endl << "Entered Matrix: " << endl;
+    for(i = 0; i < row; ++i)
+        for(j = 0; j < column; ++j)
+        {
+            cout << " " << p[i][j];
+            if(j == column - 1)
+                cout << endl << endl;
+        }
+    for(i = 0; i < row; ++i)
+        for(j = 0; j < column; ++j)
+        {
+            trans[j][i]=p[i][j];
+        }
+    cout << endl << "Transpose of Matrix: " << endl;
+    for(i = 0; i < column; ++i)
+        for(j = 0; j < row; ++j)
+        {
+            cout << " " << trans[i][j];
+            if(j == row - 1)
+                cout << endl << endl;
+        }
+    return 0;
+}
