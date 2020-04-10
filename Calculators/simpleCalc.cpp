5d3d0345bcf21ceb1dd24a20f3bebc1bd244b8ce
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-04-10 15:34:39 +0800

    Update simpleCalc.cpp

--- Calculators/simpleCalc.cpp
+++ Calculators/simpleCalc.cpp
@@ -1,1 +1,28 @@
-
+# include <iostream>
+using namespace std;
+int main(){
+    char operat;
+    float num1, num2;
+    cout << "Enter number one: ";
+    cin >> num1;
+    cout << "Enter number two: ";
+    cin >> num2;
+    cout << "Enter operator either (+*-/): ";
+    cin >> operat;
+    switch(operat)
+    {
+        case '+':
+            cout << num1+num2;
+            break;
+        case '*':
+            cout << num1*num2;
+            break;
+        case '-':
+            cout << num1-num2;
+            break;
+        case '/':
+            cout << num1/num2;
+            break;
+    }
+    return 0;
+}
