5c352fe246ac9e336f464c01bbdc654270a7a71d
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-04-10 15:36:37 +0800

    Update OneDiceRoll.cpp

--- Dice/OneDiceRoll.cpp
+++ Dice/OneDiceRoll.cpp
@@ -1,1 +1,10 @@
-
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main(){
+  for (int i=0;i<5;i++){
+    int dig = (int) (1+rand()%6);
+    cout << dig << endl;
+  }
+  return 0;
+}
