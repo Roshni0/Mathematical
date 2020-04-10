d89b09917efab7ce41a80231c114623b985cc51f
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-04-10 15:37:25 +0800

    Update TwoDiceRoll.cpp

--- Dice/TwoDiceRoll.cpp
+++ Dice/TwoDiceRoll.cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main(){
+  for (int i = 0; i<10; i++){
+  int diceOne = (int) (1+rand()%6);
+  int diceTwo = (int) (1+rand()%6);
+  int diceTotal = diceOne + diceTwo;
+  cout << diceTotal << endl;
+  }
+  return 0;
+}
