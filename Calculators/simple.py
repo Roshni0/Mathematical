64ccaf26ba6982d8f8eabcfb84f511f1a4ff1b38
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-04-10 15:35:13 +0800

    Update simple.py

--- Calculators/simple.py
+++ Calculators/simple.py
@@ -1,1 +1,67 @@
+from Tkinter import Tk
+from Tkinter import StringVar,Entry,Button
+class calculator:
+    def __init__(self):
+        window=Tk()
+        window.title('Calculator')
+        window.configure(background="black")
+        self.string=StringVar()
+        entry=Entry(window,textvariable=self.string)
+        entry.grid(row=0,column=0,columnspan=20)
+        entry.configure(background="white")
+        entry.focus()        
+        values=["7","8","9","/","%","clear",
+                "4","5","6","*","+","AC",
+                "1","2","3","-","0","="]
+        text=1
+        i=0
+        row=1
+        col=0
+        for txt in values:
+            padx=5
+            pady=5
+            if(i==6):
+                row=2
+                col=0
+            if(i==12):
+                row=3
+                col=0
+            if(i==18):
+                row=4
+                col=0
+            
+            if(txt=='='):
+                btn=Button(window,height=2,width=4,padx=50,pady=pady,text=txt,command=lambda txt=txt:self.equals())
+                btn.grid(row=row,column=col,columnspan=3,padx=1,pady=1)
+                btn.configure(background="red")
+            elif(txt=='clear'):
+                btn=Button(window,height=2,width=4,padx=padx,pady=pady, text=txt ,command=lambda txt=txt:self.delete())
+                btn.grid(row=row,column=col,padx=1,pady=1)
+                btn.configure(background="yellow")
+            elif(txt=='AC'):
+                btn=Button(window,height=2,width=4,padx=padx,pady=pady,text=txt,command=lambda txt=txt:self.clearall())
+                btn.grid(row=row,column=col,padx=1,pady=1)
+                btn.configure(background="red")
+            else:
+                btn=Button(window,height=2,width=4,padx=padx,pady=pady,text=txt ,command=lambda txt=txt:self.addChar(txt))
+                btn.grid(row=row,column=col,padx=1,pady=1)
+                btn.configure(background="grey")
+            col=col+1
+            i=i+1
+        window.mainloop()      
+    def clearall(self):
+        self.string.set("")
 
+    def equals(self):
+        result=""
+        try:
+            result=eval(self.string.get())
+            self.string.set(result)
+        except:
+            result="NOT POSSIBLE"
+        self.string.set(result)
+    def addChar(self,char):
+        self.string.set(self.string.get()+(str(char)))
+    def delete(self):
+        self.string.set(self.string.get()[0:-1])
+calculator()
