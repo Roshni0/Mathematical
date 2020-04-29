4691a45827813a73bd7ebeea2b352f49dd32c934
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-04-29 20:57:46 +0800

    Update Sum_Of_Primes_In_Range.py

--- Primes/Sum_Of_Primes_In_Range.py
+++ Primes/Sum_Of_Primes_In_Range.py
@@ -1,1 +1,11 @@
-
+lower = int(input("Enter lower bound"))
+upper = int(input("Enter upper bound"))
+sum = 0
+for num in range(lower, upper + 1):
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           sum = sum + num
+print("sum of primes in between ", lower, " and ", upper, "is",sum)
