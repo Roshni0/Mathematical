326cb95974ca847d9a35c2e0af75f8f77fb4ca66
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-05-04 08:37:10 +0800

    Update drawYourRule.py

--- Ruler/drawYourRule.py
+++ Ruler/drawYourRule.py
@@ -1,1 +1,19 @@
-
+def draw_line(tick_length,tick_label=''):
+  line='-'*tick_length
+  if tick_label:
+    line +=''+ tick_label
+  print(line)
+def draw_interval(center_length):
+  if center_length>0:
+    if center_length>0:
+      draw_interval(center_length-1)
+      draw_line(center_length)
+      draw_interval(center_length-1)
+def draw_ruler(num_inches, major_length):
+  draw_line(major_length,'0')
+  for j in range(1,1+num_inches):
+    draw_interval(major_length-1)
+    draw_line(major_length,str(j))
+length=int(input("How long do you want?"))
+numb=int(input("Number of increments in between"))
+draw_ruler(length,numb+1)
