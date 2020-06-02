f52cc2b9f04cf8ae2fae37fda465a6ca8720a5da
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-06-02 21:57:31 +0800

    Update tempConv.py

--- TemperatureConvertor/tempConv.py
+++ TemperatureConvertor/tempConv.py
@@ -1,1 +1,13 @@
-
+temp = input("Input the  temperature you like to convert? (e.g., 45F, 102C etc.) : ")
+deg = int(temp[:-1])
+initial = temp[-1]
+if initial.upper() == "C":
+  result = int(round((9 * deg) / 5 + 32))
+  fin = "Fahrenheit"
+elif initial.upper() == "F":
+  result = int(round((deg - 32) * 5 / 9))
+  fin = "Celsius"
+else:
+  print("Input proper convention.")
+  quit()
+print("The temperature in", fin, "is", result, "degrees.")
