0989d557848bf41ebdf0185a5920af110348c6d8
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-06-08 14:21:38 +0800

    Update factorial.java

--- Factorials/factorial.java
+++ Factorials/factorial.java
@@ -1,9 +1,9 @@
 class Factorial {
 
     static int factorial( int n ) {
-        if (n != 0)  // termination condition
-            return n * factorial(n-1); // recursive call
-        else
+        if (n != 0) //general case
+            return n * factorial(n-1);
+        else //base case
             return 1;
     }
 
