d98471fd44d8d180cf2766fe3b7e236f78738390
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2020-06-08 14:21:50 +0800

    Update factorial.java

--- Factorials/factorial.java
+++ Factorials/factorial.java
@@ -1,12 +1,10 @@
 class Factorial {
-
     static int factorial( int n ) {
         if (n != 0) //general case
             return n * factorial(n-1);
         else //base case
             return 1;
     }
-
     public static void main(String[] args) {
         int number = 4, result;
         result = factorial(number);
